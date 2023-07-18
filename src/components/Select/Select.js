42a9a31d6caf7e1021d2d9784197e05eaed6bb4b
Konrad Miekus <miekus.konrad@gmail.com>
2023-07-18 08:12:15 +0200

    Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,64 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
-};
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper>
+          <Icon id='chevron-down' strokeWidth={1} size={24} />
+        </IconWrapper>
+      </PresentationalBit>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`
+
+const PresentationalBit = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 16px;
+  padding-right: 52px;
+  font-size: 1rem;
+  color: ${COLORS.gray700};
+  border-radius: 8px;
+  padding-right: 52px;
+
+  // adjasent sibling selector
+  // if the native select is focused, then the presentational bit (current element) should be focused. The focus is basically  the outline
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121; 
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+   color: ${COLORS.black};
+  }
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  right: 10px;
+  bottom: 0;
+  width: 24px;
+  height: 24px;
+  margin: auto;
+  pointer-events: none;
+`
 
 export default Select;
